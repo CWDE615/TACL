4ae149b217824db7380e4445bceb789de6ac0cbd
Christopher Driggers-Ellis <cwellis001@gmail.com>
2021-04-03 19:49:59 -0400

    edited a comment

--- Map.hpp
+++ Map.hpp
@@ -21,8 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-This project was created by the members of Lucky13 for our final project in COP3530.
-To request a feature or report bugs, please use our gitHub page.
 */
 
 #pragma once
